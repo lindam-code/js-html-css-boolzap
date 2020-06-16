1a2ff81aa5712b1db9a444e2c25a0ce9fd3de5f8
lindam <linda.minotti2@gmail.com>
2020-06-16 17:42:31 +0200

    Javascript per aggiungere messaggi.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-  $('.rosso').addClass('sfondo');
+
+  // Aggiungere messaggi alla chat quando clicco icona aeroplanino
+  $('#submit-message').click(function(){
+    var message = $('.message').val();
+    var cloneMessage = $('.template .single-message').clone();
+    cloneMessage.child('p').text(message);
+    console.log(cloneMessage);
+    $('.message').val('cloneMessage');
+    $('.chat-container').append()
+  });
 
 
 
