f68149fc6415d2652e23e1063dd3a896d5df6cb9
lindam <linda.minotti2@gmail.com>
2020-06-16 18:02:38 +0200

    Javascript per aggiungere messaggi: sia cliccando invio che l'icona aeroplanino.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
 $(document).ready(function(){
 
-  // Aggiungere messaggi alla chat quando clicco icona aeroplanino
+  // Aggiungere messaggi alla chat cliccando icona aeroplanino
   $('#submit-message').click(function(){
-    var message = $('.message').val();
-    var cloneMessage = $('.template .single-message').clone();
-    cloneMessage.child('p').text(message);
-    console.log(cloneMessage);
-    $('.message').val('cloneMessage');
-    $('.chat-container').append()
+    var message = $('#message-input').val();
+    var cloneMessage = $('.template > .single-message').clone();
+    cloneMessage.find('.message-text').append(message);
+    cloneMessage.addClass('sent-message');
+    $('.chat-container').append(cloneMessage);
+    $('#message-input').val('');
   });
 
-
-
-
-
-
+  // Aggiungere messaggi alla chat cliccando invio
+  $('#message-input').keypress(function(event){
+    if (event.which === 13) {
+      var message = $('#message-input').val();
+      var cloneMessage = $('.template > .single-message').clone();
+      cloneMessage.find('.message-text').append(message);
+      cloneMessage.addClass('sent-message');
+      $('.chat-container').append(cloneMessage);
+      $('#message-input').val('');
+    }
+  });
 
 });
