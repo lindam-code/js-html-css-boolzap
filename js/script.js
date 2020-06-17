7f18fe2ba40c5a35edcc3799b840c7aeefa6c439
lindam <linda.minotti2@gmail.com>
2020-06-17 16:41:36 +0200

    Creata risposta automatica dell'interlocutore dopo un secondo.

--- js/script.js
+++ js/script.js
@@ -3,17 +3,25 @@ $(document).ready(function(){
   // Aggiungere messaggi alla chat cliccando icona aeroplanino
   $('#submit-message').click(function(){
     sendMessage();
+    // Invia una risposta dell'interlocutore dopo un secondo
+    setTimeout(function(){
+      sendAnswer();
+  }, 1000);
   });
 
   // Aggiungere messaggi alla chat cliccando invio
   $('#message-input').keypress(function(event){
     if (event.which === 13) {
       sendMessage();
+      // Invia una risposta dell'interlocutore dopo un secondo
+      setTimeout(function(){
+        sendAnswer();
+    }, 1000);
     }
   });
 
   // Funzioni
-  // Funzione che invia il messaggio dell'utente nella charset
+  // Funzione che invia il messaggio dell'utente nella chat
   function sendMessage() {
     var message = $('#message-input').val();
     if (message != '') {
@@ -35,8 +43,27 @@ $(document).ready(function(){
       $('.chat-wrapper').scrollTop($('.chat-wrapper').height());
       // Pulisco l'input
       $('#message-input').val('');
-
     };
+  };
+
+  // Funzione per inviare la risposta dell'iterlocutore
+  function sendAnswer() {
+    // Clona template messaggio
+    var cloneMessage = $('.template > .single-message').clone();
+    // Aggiunge il testo del messaggio
+    cloneMessage.find('.text-message').append('Ok!');
+    // Aggiunge la classe messaggio ricevuto
+    cloneMessage.addClass('received-message');
+    // Inserisce orario del messaggio
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var currentTime = addZeroToNumber(hours) + ':' + addZeroToNumber(minutes);
+    cloneMessage.find('.time-message').text(currentTime);
+    // Appende al contenitore della chat il messaggio
+    $('.chat-container').append(cloneMessage);
+    // Scrollo all'ultimo messaggio inviato
+    $('.chat-wrapper').scrollTop($('.chat-wrapper').height());
   };
 
   // Funzione per aggiungere zero ad un numero quando è minore di 10
