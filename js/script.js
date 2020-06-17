d6e8463c5d1980d12543e71720042635e2ed9048
lindam <linda.minotti2@gmail.com>
2020-06-17 16:14:48 +0200

    Sistemato codice.

--- js/script.js
+++ js/script.js
@@ -2,24 +2,49 @@ $(document).ready(function(){
 
   // Aggiungere messaggi alla chat cliccando icona aeroplanino
   $('#submit-message').click(function(){
-    var message = $('#message-input').val();
-    var cloneMessage = $('.template > .single-message').clone();
-    cloneMessage.find('.message-text').append(message);
-    cloneMessage.addClass('sent-message');
-    $('.chat-container').append(cloneMessage);
-    $('#message-input').val('');
+    sendMessage();
   });
 
   // Aggiungere messaggi alla chat cliccando invio
   $('#message-input').keypress(function(event){
     if (event.which === 13) {
-      var message = $('#message-input').val();
-      var cloneMessage = $('.template > .single-message').clone();
-      cloneMessage.find('.message-text').append(message);
-      cloneMessage.addClass('sent-message');
-      $('.chat-container').append(cloneMessage);
-      $('#message-input').val('');
+      sendMessage();
     }
   });
+
+  // Funzioni
+  // Funzione che invia il messaggio dell'utente nella charset
+  function sendMessage() {
+    var message = $('#message-input').val();
+    if (message != '') {
+      // Clona template messaggio
+      var cloneMessage = $('.template > .single-message').clone();
+      // Aggiunge il testo del messaggio
+      cloneMessage.find('.text-message').append(message);
+      // Aggiunge la classe messaggio inviato
+      cloneMessage.addClass('sent-message');
+      // Inserisce orario del messaggio
+      var date = new Date();
+      var hours = date.getHours();
+      var minutes = date.getMinutes();
+      var currentTime = addZeroToNumber(hours) + ':' + addZeroToNumber(minutes);
+      cloneMessage.find('.time-message').text(currentTime);
+      // Appende al contenitore della chat il messaggio
+      $('.chat-container').append(cloneMessage);
+      // Scrollo all'ultimo messaggio inviato
+      $('.chat-wrapper').scrollTop($('.chat-wrapper').height());
+      // Pulisco l'input
+      $('#message-input').val('');
+
+    };
+  };
+
+  // Funzione per aggiungere zero ad un numero quando è minore di 10
+  function addZeroToNumber(number) {
+    if (number < 10) {
+      return '0' + number;
+    }
+    return number;
+  };
 
 });
